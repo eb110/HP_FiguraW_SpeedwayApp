2238cea4b3be3cd67e47522ece80f7f69ed34efa
eb110 <wfigura@op.pl>
2021-02-23 16:49:04 +0000

    finished algorithm

--- ajax/algorithm.js
+++ ajax/algorithm.js
@@ -3,12 +3,14 @@ let teamB = []
 let speedwayRiders = []
 let tablePositions1 = []
 let tablePositionsMainPrintout = []
+let startingFlag = 0
 
 $(function () {
     $('#get-result').on('click', () => {
         event.preventDefault();
         teamA = playersDataCollect(1)
         teamB = playersDataCollect(9)
+        temporaryData(teamA, teamB)
         speedwayRiders = teamA.concat(teamB)
         createInitialTablePositions()
         let result = [teamA, teamB]
@@ -29,9 +31,6 @@ $(function () {
             url: '/resultTable',
             contentType: 'application/json',
             success: (response) => {
-                let riders = response.riders
-                let l = riders.length
-                let wsad = []
                 createMainTablePrintout()
                 let wsad2 = tablePositionsMainPrintout.join('')
                 let tbodyEL = $('#table')
@@ -53,7 +52,9 @@ $(function () {
                 let wsad = ''
                 for(let i = 0; i < 8; i++){
                     let a = team[i].name
-                    let temp = '<div id="tableInside"><label>' + a +
+                    let temp = '<div id="tableInside"><label>' + a + ' ' + team[i].points[0] +
+                    ' ' + team[i].points[1] + ' ' + team[i].points[2] + ' ' + team[i].points[3] +
+                    ' ' + team[i].points[4] +
                     '</label></div>'
                     wsad += temp
                 }
@@ -75,7 +76,9 @@ $(function () {
                 let wsad = ''
                 for(let i = 0; i < 8; i++){
                     let a = team[i].name
-                    let temp = '<div id="tableInside"><label>' + a +
+                    let temp = '<div id="tableInside"><label>' + a + ' ' + team[i].points[0] +
+                    ' ' + team[i].points[1] + ' ' + team[i].points[2] + ' ' + team[i].points[3] +
+                    ' ' + team[i].points[4] +
                     '</label></div>'
                     wsad += temp
                 }
@@ -88,7 +91,31 @@ $(function () {
     })
 })
 
+function createUpdatedTablePositions(){
+    for(let i = 0; i < 16; i++)speedwayRiders[i].position = 0
+    tablePositions1 = []
+    for(let i = 0; i < 13; i++){
+        tablePositions1.push([])
+        for(let j = 0; j < 4; j++){
+            tablePositions1[i].push({})
+            let temp = '#h' + i + 'r' + j
+            let wsad = $(temp).val()
+            console.log(wsad)
+            for(let k = 0; k < 16; k++){
+                if(wsad.includes(speedwayRiders[k].name)){
+                    tablePositions1[i][j].name = speedwayRiders[k].name
+                    tablePositions1[i][j].points = speedwayRiders[k].points[speedwayRiders[k].position++]
+                    break
+                }
+            }
+        }
+        console.log(tablePositions1[i])
+    }
+}
+
 function createMainTablePrintout(){
+    if(startingFlag == 0)startingFlag = 1
+    else createUpdatedTablePositions()
     for(let i = 0; i < 8; i++){
         teamA[i].position = 0
         teamB[i].position = 0
@@ -97,7 +124,7 @@ function createMainTablePrintout(){
     for (let i = 0; i < 13; i++) {
         tablePositionsMainPrintout.push('<div id="tableDiv">')
         for (let k = 0; k < 4; k++) {
-            tablePositionsMainPrintout.push('<select id="h' + i + 'r' + k + '">')
+            tablePositionsMainPrintout.push('<select id="h' + i + 'r' + k + '"><optgroup>')
             tablePositionsMainPrintout.push('<option value="' + tablePositions1[i][k].name + '">' + 
             tablePositions1[i][k].name + '</option>')  
             let inlet = reserveRidersQueue(tablePositions1[i][k].name)
@@ -105,10 +132,9 @@ function createMainTablePrintout(){
                 tablePositionsMainPrintout.push('<option value="' + inlet[j].name + ' (' + inlet[j].point + ') ' + '">' + 
                 inlet[j].name + ' (' + inlet[j].point + ') ' + '</option>')                      
             }
-            tablePositionsMainPrintout.push('</select>')
+            tablePositionsMainPrintout.push('</optgroup></select>')
             tablePositionsMainPrintout.push('<input type="text" value="' + tablePositions1[i][k].points + '">')
         }
-        tablePositionsMainPrintout.push('<button id="#update-button">Update</button>')
         tablePositionsMainPrintout.push('</div>')
     }
 }
@@ -122,7 +148,7 @@ function reserveRidersQueue(rider){
         if(team[i].name == rider) {
             team[i].position++
         }
-        else if(team[i].position < 5){
+        else if(team[i].position < 5 && team[i].points[team[i].position] != '-'){
             let wsad = {}
             wsad.name = team[i].name
             wsad.point = team[i].points[team[i].position]
@@ -146,6 +172,121 @@ function playersDataCollect(range) {
         }
     }
     return team
+}
+
+function temporaryData(a, b){
+    a[0].position = 0
+    a[0].name = 'pociejkowicz konstanty'
+    a[0].points[0] = 3
+    a[0].points[1] = 3
+    a[0].points[2] = 3
+    a[0].points[3] = 3
+    a[0].points[4] = '-'
+    a[1].position = 0
+    a[1].name = 'bruzda piotr'
+    a[1].points[0] = 2
+    a[1].points[1] = 2
+    a[1].points[2] = 2
+    a[1].points[3] = 2
+    a[1].points[4] = '-'
+    a[2].position = 0
+    a[2].name = 'trzeszkowski jerzy'
+    a[2].points[0] = 3
+    a[2].points[1] = 3
+    a[2].points[2] = 3
+    a[2].points[3] = 3
+    a[2].points[4] = '-'
+    a[3].position = 0
+    a[3].name = 'domiszewski andrzej'
+    a[3].points[0] = 'u'
+    a[3].points[1] = '-'
+    a[3].points[2] = '-'
+    a[3].points[3] = '-'
+    a[3].points[4] = '-'
+    a[4].position = 0
+    a[4].name = 'jaroszewicz bohdan'
+    a[4].points[0] = 'w'
+    a[4].points[1] = 3
+    a[4].points[2] = 3
+    a[4].points[3] = 2
+    a[4].points[4] = '-'
+    a[5].position = 0
+    a[5].name = 'antos zygmunt'
+    a[5].points[0] = 3
+    a[5].points[1] = 1
+    a[5].points[2] = 1
+    a[5].points[3] = '-'
+    a[5].points[4] = '-'
+    a[6].position = 0
+    a[6].name = 'słaboń adolf'
+    a[6].points[0] = 3
+    a[6].points[1] = 3
+    a[6].points[2] = 2
+    a[6].points[3] = '-'
+    a[6].points[4] = '-'
+    a[7].position = 0
+    a[7].name = 'nowak stanisław'
+    a[7].points[0] = 2
+    a[7].points[1] = 1
+    a[7].points[2] = 1
+    a[7].points[3] = '-'
+    a[7].points[4] = '-'
+    b[0].position = 0
+    b[0].name = 'malinowski jan'
+    b[0].points[0] = 1
+    b[0].points[1] = 2
+    b[0].points[2] = 'd'
+    b[0].points[3] = 2
+    b[0].points[4] = 1
+    b[1].position = 0
+    b[1].name = 'lalicki stanisław'
+    b[1].points[0] = 0
+    b[1].points[1] = 1
+    b[1].points[2] = 'd'
+    b[1].points[3] = 0
+    b[1].points[4] = '-'
+    b[2].position = 0
+    b[2].name = 'kolber jan'
+    b[2].points[0] = 2
+    b[2].points[1] = 'w'
+    b[2].points[2] = 2
+    b[2].points[3] = 1
+    b[2].points[4] = '-'
+    b[3].position = 0
+    b[3].name = 'stawecki marian'
+    b[3].points[0] = 1
+    b[3].points[1] = 1
+    b[3].points[2] = 2
+    b[3].points[3] = 'd'
+    b[3].points[4] = '-'
+    b[4].position = 0
+    b[4].name = 'batko józef'
+    b[4].points[0] = 2
+    b[4].points[1] = 1
+    b[4].points[2] = 'u'
+    b[4].points[3] = '-'
+    b[4].points[4] = '-'
+    b[5].position = 0
+    b[5].name = 'spychała marian'
+    b[5].points[0] = 1
+    b[5].points[1] = 0
+    b[5].points[2] = 0
+    b[5].points[3] = '-'
+    b[5].points[4] = '-'
+    b[6].position = 0
+    b[6].name = 'krajewski marian'
+    b[6].points[0] = 0
+    b[6].points[1] = 0
+    b[6].points[2] = 0
+    b[6].points[3] = '-'
+    b[6].points[4] = '-'
+    b[7].position = 0
+    b[7].name = ''
+    b[7].points[0] = '-'
+    b[7].points[1] = '-'
+    b[7].points[2] = '-'
+    b[7].points[3] = '-'
+    b[7].points[4] = '-'
 }
 
 function createInitialTablePositions()
